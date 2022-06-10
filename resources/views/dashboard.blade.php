9e29bf10d837e3c9c058710b3979668db3c7a13d
Yassir Ah <yassirnugroho48@gmail.com>
2022-06-10 22:41:33 +0700

    Auth  changes views & add dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,60 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('layouts.modern')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+@section('modern-sidebar')
+@include('layouts.modern-sidebar')
+@endsection
+
+@section('content')
+<div class="container px-6 mx-auto grid">
+    <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
+        Dashboard
+    </h2>
+    <div class="container px-6 mx-auto grid">
+        <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
+            Your Inteaction Grafik
+        </h2>
+
+        <div class="grid gap-6 mb-8 md:grid-cols-2">
+            <!-- Lines chart -->
+            <div class="min-w-0 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+                <h4 class="mb-4 font-semibold text-gray-800 dark:text-gray-300">
+                    Lines
+                </h4>
+                <canvas id="line"></canvas>
+                <div class="flex justify-center mt-4 space-x-3 text-sm text-gray-600 dark:text-gray-400">
+                    <!-- Chart legend -->
+                    <div class="flex items-center">
+                        <span class="inline-block w-3 h-3 mr-1 bg-teal-500 rounded-full"></span>
+                        <span>Question</span>
+                    </div>
+                    <div class="flex items-center">
+                        <span class="inline-block w-3 h-3 mr-1 bg-purple-600 rounded-full"></span>
+                        <span>Comments</span>
+                    </div>
                 </div>
             </div>
         </div>
+        <div class="emoji">
+            <h4 class="mb-4 font-semibold text-center text-gray-800 dark:text-gray-300">
+                Rate us
+            </h4>
+            <ul class="flex justify-center">
+                <li>
+                    <svg class="h-8 w-8" fill="#f22613" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                </li>
+                <li>
+                    <svg class="h-8 w-8" fill="#00e640" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                </li>
+            </ul>
+        </div>
     </div>
-</x-app-layout>
+
+
+</div>
+@endsection

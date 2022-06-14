f20d9c4d38960db72e9ef87316346d12be77d947
Yassir Ah <yassirnugroho48@gmail.com>
2022-06-14 18:23:04 +0700

    add custom validation alpha_spaces

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,9 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +26,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useTailwind();
+
+        //Add this custom alpha space
+        Validator::extend('alpha_spaces', function ($attribute, $value) {
+
+            // This will only accept alpha and spaces. 
+            // If you want to accept hyphens use: /^[\pL\s-]+$/u.
+            return preg_match('/^[\pL\s]+$/u', $value);
+        });
     }
 }
--- lang/en/validation.php
+++ lang/en/validation.php
@@ -13,6 +13,11 @@ return [
     |
     */
 
+    // Costum
+    'alpha_spaces'         => 'The :attribute may only contain letters and spaces.',
+
+
+    // Original
     'accepted' => 'The :attribute must be accepted.',
     'accepted_if' => 'The :attribute must be accepted when :other is :value.',
     'active_url' => 'The :attribute is not a valid URL.',
